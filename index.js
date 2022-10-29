f25b0839a74bb82f5566b599dab4c4c86b6fd4d6
st-pardon <okc4pardon@gmail.com>
2022-10-29 17:05:36 +0100

    :hammer: updated index and added new dependencies

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-require("dotenv").config;
+require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./config/db.config");
 const { userRoute } = require("./routes/user.route");
 const { postRoute } = require("./routes/post.route");
 const { signinRoute, signupRoute } = require("./routes/authentication.route");
+require("./middleware/auth.middleware");
 
 // initialize server
 const app = express();
-
 // parse body data
-app.use(express.json());
+app
+  .use(express.urlencoded({ extended: false }))
+  .use("/users", userRoute)
+  .use("/", signupRoute)
+  .use("/signin", signinRoute)
+  .use("/posts", postRoute);
 
 app.get("/", (req, res) => {
   res.end(
     "Welcome to your blog, please make req to the /login route to login or to the /blog to get blog article. Thank you!"
   );
 });
-
-app
-  .use("/users", userRoute)
-  .use("/signup", signupRoute)
-  .use("/signin", signinRoute)
-  .use("/posts", postRoute);
-
 // connect To Mongo DB
 connectToMongoDB();
 // start server
