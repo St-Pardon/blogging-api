d22d4b56a150d3c46bdf7af20f9f5d3f530ef8f5
st-pardon <okc4pardon@gmail.com>
2022-10-29 17:03:20 +0100

    :wrench: modified signup route

--- routes/authentication.route.js
+++ routes/authentication.route.js
@@ -1,22 +1,22 @@
 const { Router } = require("express");
+const passport = require("passport");
 const { userModel } = require("../models/user.model");
 
 const signinRoute = Router();
 const signupRoute = Router();
 
-signinRoute.post("/", (req, res) => {
-  try {
-    const user = req.body;
-    userModel.create(user).then((user) => {
-      res.status(200).send(user);
-    });
-  } catch (err) {
-    res.status(401).send(`An error occured -> ${err}`);
+signupRoute.post(
+  "/signup",
+  passport.authenticate("signup", { session: false }),
+  async (req, res, next) => {
+      res.status(200).json({
+        message: "Signup successful",
+        user: req.user,
+      });
   }
-});
+);
 
-
-signupRoute.post("/", (req, res) => {
+signinRoute.post("/", (req, res) => {
   try {
     const user = req.body;
     userModel.create(user).then((user) => {
