cc337e55050894b84ef459adc94a03efcf909725
st-pardon <okc4pardon@gmail.com>
2022-10-30 09:59:53 +0100

    :hammer: added login authentication

--- routes/authentication.route.js
+++ routes/authentication.route.js
@@ -1,30 +1,42 @@
 const { Router } = require("express");
 const passport = require("passport");
-const { userModel } = require("../models/user.model");
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const signinRoute = Router();
 const signupRoute = Router();
 
 signupRoute.post(
-  "/signup",
+  "/",
   passport.authenticate("signup", { session: false }),
   async (req, res, next) => {
-      res.status(200).json({
-        message: "Signup successful",
-        user: req.user,
-      });
+    res.status(200).json({
+      message: "Signup successful",
+      user: req.user,
+    });
   }
 );
 
-signinRoute.post("/", (req, res) => {
-  try {
-    const user = req.body;
-    userModel.create(user).then((user) => {
-      res.status(200).send(user);
-    });
-  } catch (err) {
-    res.status(401).send(`An error occured -> ${err}`);
-  }
+signinRoute.post("/", async (req, res, next) => {
+  passport.authenticate("signin", async (err, user, info) => {
+    try {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        const error = new Error("Username or password is incorrect");
+        return next(error);
+      }
+      req.login(user, { session: false }, async (error) => {
+        if (error) return next(error);
+        const body = { _id: user._id, email: user.email };
+        const token = jwt.sign({ user: body }, process.env.JWT_SECRET);
+        return res.json({ token });
+      });
+    } catch (error) {
+      return next(error);
+    }
+  })(req, res, next);
 });
 
 module.exports = { signinRoute, signupRoute };
