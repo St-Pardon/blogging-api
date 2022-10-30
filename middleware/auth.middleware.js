1783408c7cb5513fec5a1b79b877fa39cfc9a10a
st-pardon <okc4pardon@gmail.com>
2022-10-30 10:05:20 +0100

    :hammer: updated auth middlieware for passport login strategy

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -33,10 +33,39 @@ passport.use(
     async (req, email, password, done) => {
       try {
         const data = req.body;
-        const user = await userModel.create({...data, email, password });
+        const user = await userModel.create({ ...data, email, password });
         return done(null, user);
       } catch (error) {
         done(error);
+      }
+    }
+  )
+);
+
+passport.use(
+  "signin",
+  new localStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    async (email, password, done) => {
+      try {
+        const user = await userModel.findOne({ email });
+
+        if (!user) {
+          return done(null, false, { message: "User not found" });
+        }
+
+        const validate = await user.isValidPassword(password);
+
+        if (!validate) {
+          return done(null, false, { message: "Wrong Password" });
+        }
+
+        return done(null, user, { message: "Logged in Successfully" });
+      } catch (error) {
+        return done(error);
       }
     }
   )
