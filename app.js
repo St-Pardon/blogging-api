886899ad1f653e7945f0486d17174125101be3f8
st-pardon <okc4pardon@gmail.com>
2022-11-02 19:39:15 +0100

    :hammer: install packages and refactor index.js

---
+++ app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const express = require("express");
+const { userRoute } = require("./routes/user.route");
+const { postRoute } = require("./routes/post.route");
+const { signinRoute, signupRoute } = require("./routes/authentication.route");
+const { errHandler } = require("./middleware/errhandle.middleware");
+require("./middleware/auth.middleware");
+
+// initialize server
+const app = express();
+
+app
+  // body parsing middleware
+  .use(express.urlencoded({ extended: false }))
+  .use(express.json())
+
+  // routes handlers
+  .use("/users", userRoute)
+  .use("/signup", signupRoute)
+  .use("/signin", signinRoute)
+  .use("/posts", postRoute)
+
+  // error handling middleware
+  .use(errHandler)
+
+  // welcome page route
+  .get("/", (req, res) => {
+    res.end(
+      "Welcome to your blog, please make req to the /signin route to login or /signup if you don't have an account. explore the /posts to get blog article. Thank you!"
+    );
+  });
+
+module.exports = app;
--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-require("dotenv").config();
-const express = require("express");
+const app = require('./app')
 const { connectToMongoDB } = require("./config/db.config");
-const { userRoute } = require("./routes/user.route");
-const { postRoute } = require("./routes/post.route");
-const { signinRoute, signupRoute } = require("./routes/authentication.route");
-const { errHandler } = require("./middleware/errhandle.middleware");
-require("./middleware/auth.middleware");
-
-// initialize server
-const app = express();
-
-app
-  // body parsing middleware
-  .use(express.urlencoded({ extended: false }))
-  .use(express.json())
-
-  // routes handlers
-  .use("/users", userRoute)
-  .use("/signup", signupRoute)
-  .use("/signin", signinRoute)
-  .use("/posts", postRoute)
-
-  // error handling middleware
-  .use(errHandler)
-
-  // welcome page route
-  .get("/", (req, res) => {
-    res.end(
-      "Welcome to your blog, please make req to the /login route to login or to the /post to get blog article. Thank you!"
-    );
-  });
 
 // connect To Mongo DB
 connectToMongoDB();
