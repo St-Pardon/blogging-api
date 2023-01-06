f927ea9a34064ec08c4808b9b27864097bf439a3
st-pardon <okc4pardon@gmail.com>
2023-01-06 19:18:19 +0100

    :hammer: versioned api

--- app.js
+++ app.js
@@ -8,17 +8,25 @@ require("./middleware/auth.middleware");
 
 // initialize server
 const app = express();
+const BaseRouteV1 = "/api/v1/";
+const BaseRouteV2 = "/api/v2/";
 
 app
   // body parsing middleware
   .use(express.urlencoded({ extended: false }))
   .use(express.json())
 
-  // routes handlers
-  .use("/users", userRoute)
-  .use("/signup", signupRoute)
-  .use("/signin", signinRoute)
-  .use("/posts", postRoute)
+  // routes handlers v1
+  .use(`${BaseRouteV1}/users`, userRoute)
+  .use(`${BaseRouteV1}/signup`, signupRoute)
+  .use(`${BaseRouteV1}/signin`, signinRoute)
+  .use(`${BaseRouteV1}/posts`, postRoute)
+
+  // routes handlers v2
+  .use(`${BaseRouteV2}/users`, userRoute)
+  .use(`${BaseRouteV2}/signup`, signupRoute)
+  .use(`${BaseRouteV2}/signin`, signinRoute)
+  .use(`${BaseRouteV2}/posts`, postRoute)
 
   // error handling middleware
   .use(errHandler)
@@ -28,7 +36,21 @@ app
     res
       .status(200)
       .end(
-        "Welcome to your blog, please make req to the /signin route to login or /signup if you don't have an account. explore the /posts to get blog article. Thank you!"
+        "Welcome to your blog, use version 1 on /api/v1 or version 2 on /api/v2 Thank you!"
+      );
+  })
+  .get(`${BaseRouteV1}/v1`, (req, res) => {
+    res
+      .status(200)
+      .end(
+        "API Version 1, please make req to the /signin route to login or /signup if you don't have an account. explore the /posts to get blog article. Check out API documentation for more routes Thank you!"
+      );
+  })
+  .get(`${BaseRouteV2}/v2`, (req, res) => {
+    res
+      .status(200)
+      .end(
+        "API Version 2, please make req to the /signin route to login or /signup if you don't have an account. explore the /posts to get blog article. Check out API documentation for more routes Thank you!"
       );
   })
 
