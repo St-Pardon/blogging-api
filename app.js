d328ffd8e86c92a8690ecfd9a48e42e3be28532f
St Pardon <okc4pardon@gmail.com>
2023-03-14 20:08:52 +0100

    :hammer: updated response message

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-require("dotenv").config();
-const express = require("express");
-const { userRoute } = require("./routes/v1/user.route");
-const { postRoute } = require("./routes/v1/post.route");
-const { signinRoute, signupRoute } = require("./routes/v1/authentication.route");
-const { errHandler } = require("./middleware/errhandle.middleware");
-require("./middleware/auth.middleware");
+require('dotenv').config();
+const express = require('express');
+const { userRoute } = require('./routes/v1/user.route');
+const { postRoute } = require('./routes/v1/post.route');
+const {
+  signinRoute,
+  signupRoute,
+} = require('./routes/v1/authentication.route');
+const { errHandler } = require('./middleware/errhandle.middleware');
+require('./middleware/auth.middleware');
 
 // initialize server
 const app = express();
-const BaseRouteV1 = "/api/v1";
-const BaseRouteV2 = "/api/v2";
+const BaseRouteV1 = '/api/v1';
+const BaseRouteV2 = '/api/v2';
 
 app
   // body parsing middleware
@@ -32,31 +35,27 @@ app
   .use(errHandler)
 
   // welcome page route
-  .get("/", (req, res) => {
+  .get('/', (req, res) => {
     res
       .status(200)
       .end(
-        "Welcome to your blog, use version 1 on /api/v1 or version 2 on /api/v2 Thank you!"
+        'Welcome to your blog, use version 1 on /api/v1 or version 2 on /api/v2 Thank you!'
       );
   })
   .get(`${BaseRouteV1}`, (req, res) => {
     res
       .status(200)
       .end(
-        "Blogging API Version 1, please make req to the /signin route to login or /signup if you don't have an account. explore the /posts to get blog article. Check out API documentation for more routes Thank you!"
+        "Blogging API Version 1, please make req to the /signin route to login or /signup if you don't have an account. explore the /posts to get blog article. Check out API documentation at https://github.com/St-Pardon/blogging-api/blob/master/routes/v1/README.md for more routes Thank you!"
       );
   })
   .get(`${BaseRouteV2}`, (req, res) => {
-    res
-      .status(200)
-      .end(
-        "Blogging API Version 2, comming soon Thank you!"
-      );
+    res.status(200).end('Blogging API Version 2, comming soon Thank you!');
   })
 
   // 404 error page
-  .get("*", (req, res) => {
-    res.status(404).end("404 not found, check route or method and try again");
+  .get('*', (req, res) => {
+    res.status(404).end('404 not found, check route or method and try again');
   });
 
 module.exports = app;
